28c6f9d73d9e385e288d89084f6d844577856324
Dwijesh Shah <dwijeshs@splunk.com>
2021-10-18 10:57:40 +0530

    test: updated splunk_sync.sh

--- splunk_sync.sh
+++ splunk_sync.sh
@@ -21,7 +21,7 @@ fi
 pip install pip --upgrade
 pip install poetry
 poetry export --without-hashes --dev -o requirements_dev.txt
-pip install -r requirements_dev.txt --use-deprecated=legacy-resolver || true
+pip install -r requirements_dev.txt
 splunk_version=$(python splunk_matrix_update.py)
 echo $splunk_version
 
